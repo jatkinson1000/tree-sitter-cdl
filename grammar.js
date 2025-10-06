9b36feee4b56475f8b7b824d93794ae724fcd29f
Jack Atkinson <jwa34@cam.ac.uk>
2025-10-06 16:47:11 +0100

    Add extras and tokens for whitespace and comments, and a placeholder for statements ending with a semicolon.

--- grammar.js
+++ grammar.js
@@ -10,6 +10,13 @@
 module.exports = grammar({
   name: 'cdl',
 
+  // Whitespace (spaces, tabs, newlines) can appear anywhere
+  // Single-line comments can appear anywhere to end a line
+  extras: $ => [
+    /\s/,
+    $.comment,
+  ],
+
   rules: {
     dataset: $ => seq(
       'netcdf',
@@ -18,10 +25,14 @@ module.exports = grammar({
       '}',
     ),
 
-    // Tokens
     // Names must start with a-zA-Z_ be non-zero length and contain a-zA-Z0-9_.+-@
     identifier: $ => /[a-zA-Z_][a-zA-Z0-9_.+\-@]*/,
 
+    // Comments exist anywhere on one line preceded by //
+    comment: $ => token(seq('//', /.*/)),
+
+    // Statements should end in a ;
+    statement: $ => seq($.identifier, ';'),
   },
 
 });
--- src/parser.c
+++ src/parser.c
@@ -9,9 +9,9 @@
 #define LANGUAGE_VERSION 15
 #define STATE_COUNT 7
 #define LARGE_STATE_COUNT 2
-#define SYMBOL_COUNT 6
+#define SYMBOL_COUNT 7
 #define ALIAS_COUNT 0
-#define TOKEN_COUNT 5
+#define TOKEN_COUNT 6
 #define EXTERNAL_TOKEN_COUNT 0
 #define FIELD_COUNT 0
 #define MAX_ALIAS_SEQUENCE_LENGTH 4
@@ -24,7 +24,8 @@ enum ts_symbol_identifiers {
   anon_sym_LBRACE = 2,
   anon_sym_RBRACE = 3,
   sym_identifier = 4,
-  sym_dataset = 5,
+  sym_comment = 5,
+  sym_dataset = 6,
 };
 
 static const char * const ts_symbol_names[] = {
@@ -33,6 +34,7 @@ static const char * const ts_symbol_names[] = {
   [anon_sym_LBRACE] = "{",
   [anon_sym_RBRACE] = "}",
   [sym_identifier] = "identifier",
+  [sym_comment] = "comment",
   [sym_dataset] = "dataset",
 };
 
@@ -42,6 +44,7 @@ static const TSSymbol ts_symbol_map[] = {
   [anon_sym_LBRACE] = anon_sym_LBRACE,
   [anon_sym_RBRACE] = anon_sym_RBRACE,
   [sym_identifier] = sym_identifier,
+  [sym_comment] = sym_comment,
   [sym_dataset] = sym_dataset,
 };
 
@@ -63,6 +66,10 @@ static const TSSymbolMetadata ts_symbol_metadata[] = {
     .named = false,
   },
   [sym_identifier] = {
+    .visible = true,
+    .named = true,
+  },
+  [sym_comment] = {
     .visible = true,
     .named = true,
   },
@@ -95,48 +102,53 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
   eof = lexer->eof(lexer);
   switch (state) {
     case 0:
-      if (eof) ADVANCE(7);
-      if (lookahead == 'n') ADVANCE(3);
-      if (lookahead == '{') ADVANCE(9);
-      if (lookahead == '}') ADVANCE(10);
+      if (eof) ADVANCE(8);
+      if (lookahead == '/') ADVANCE(2);
+      if (lookahead == 'n') ADVANCE(5);
+      if (lookahead == '{') ADVANCE(10);
+      if (lookahead == '}') ADVANCE(11);
       if (('\t' <= lookahead && lookahead <= '\r') ||
           lookahead == ' ') SKIP(0);
       END_STATE();
     case 1:
-      if (lookahead == 'c') ADVANCE(2);
-      END_STATE();
-    case 2:
-      if (lookahead == 'd') ADVANCE(4);
-      END_STATE();
-    case 3:
-      if (lookahead == 'e') ADVANCE(5);
-      END_STATE();
-    case 4:
-      if (lookahead == 'f') ADVANCE(8);
-      END_STATE();
-    case 5:
-      if (lookahead == 't') ADVANCE(1);
-      END_STATE();
-    case 6:
+      if (lookahead == '/') ADVANCE(2);
       if (('\t' <= lookahead && lookahead <= '\r') ||
-          lookahead == ' ') SKIP(6);
+          lookahead == ' ') SKIP(1);
       if (('A' <= lookahead && lookahead <= 'Z') ||
           lookahead == '_' ||
-          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(11);
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(12);
+      END_STATE();
+    case 2:
+      if (lookahead == '/') ADVANCE(13);
+      END_STATE();
+    case 3:
+      if (lookahead == 'c') ADVANCE(4);
+      END_STATE();
+    case 4:
+      if (lookahead == 'd') ADVANCE(6);
+      END_STATE();
+    case 5:
+      if (lookahead == 'e') ADVANCE(7);
+      END_STATE();
+    case 6:
+      if (lookahead == 'f') ADVANCE(9);
       END_STATE();
     case 7:
-      ACCEPT_TOKEN(ts_builtin_sym_end);
+      if (lookahead == 't') ADVANCE(3);
       END_STATE();
     case 8:
-      ACCEPT_TOKEN(anon_sym_netcdf);
+      ACCEPT_TOKEN(ts_builtin_sym_end);
       END_STATE();
     case 9:
-      ACCEPT_TOKEN(anon_sym_LBRACE);
+      ACCEPT_TOKEN(anon_sym_netcdf);
       END_STATE();
     case 10:
-      ACCEPT_TOKEN(anon_sym_RBRACE);
+      ACCEPT_TOKEN(anon_sym_LBRACE);
       END_STATE();
     case 11:
+      ACCEPT_TOKEN(anon_sym_RBRACE);
+      END_STATE();
+    case 12:
       ACCEPT_TOKEN(sym_identifier);
       if (lookahead == '+' ||
           lookahead == '-' ||
@@ -144,7 +156,12 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
           ('0' <= lookahead && lookahead <= '9') ||
           ('@' <= lookahead && lookahead <= 'Z') ||
           lookahead == '_' ||
-          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(11);
+          ('a' <= lookahead && lookahead <= 'z')) ADVANCE(12);
+      END_STATE();
+    case 13:
+      ACCEPT_TOKEN(sym_comment);
+      if (lookahead != 0 &&
+          lookahead != '\n') ADVANCE(13);
       END_STATE();
     default:
       return false;
@@ -154,7 +171,7 @@ static bool ts_lex(TSLexer *lexer, TSStateId state) {
 static const TSLexerMode ts_lex_modes[STATE_COUNT] = {
   [0] = {.lex_state = 0},
   [1] = {.lex_state = 0},
-  [2] = {.lex_state = 6},
+  [2] = {.lex_state = 1},
   [3] = {.lex_state = 0},
   [4] = {.lex_state = 0},
   [5] = {.lex_state = 0},
@@ -167,48 +184,61 @@ static const uint16_t ts_parse_table[LARGE_STATE_COUNT][SYMBOL_COUNT] = {
     [anon_sym_netcdf] = ACTIONS(1),
     [anon_sym_LBRACE] = ACTIONS(1),
     [anon_sym_RBRACE] = ACTIONS(1),
+    [sym_comment] = ACTIONS(3),
   },
   [STATE(1)] = {
     [sym_dataset] = STATE(3),
-    [anon_sym_netcdf] = ACTIONS(3),
+    [anon_sym_netcdf] = ACTIONS(5),
+    [sym_comment] = ACTIONS(3),
   },
 };
 
 static const uint16_t ts_small_parse_table[] = {
-  [0] = 1,
-    ACTIONS(5), 1,
-      sym_identifier,
-  [4] = 1,
+  [0] = 2,
+    ACTIONS(3), 1,
+      sym_comment,
     ACTIONS(7), 1,
-      ts_builtin_sym_end,
-  [8] = 1,
+      sym_identifier,
+  [7] = 2,
+    ACTIONS(3), 1,
+      sym_comment,
     ACTIONS(9), 1,
-      anon_sym_LBRACE,
-  [12] = 1,
+      ts_builtin_sym_end,
+  [14] = 2,
+    ACTIONS(3), 1,
+      sym_comment,
     ACTIONS(11), 1,
-      anon_sym_RBRACE,
-  [16] = 1,
+      anon_sym_LBRACE,
+  [21] = 2,
+    ACTIONS(3), 1,
+      sym_comment,
     ACTIONS(13), 1,
+      anon_sym_RBRACE,
+  [28] = 2,
+    ACTIONS(3), 1,
+      sym_comment,
+    ACTIONS(15), 1,
       ts_builtin_sym_end,
 };
 
 static const uint32_t ts_small_parse_table_map[] = {
   [SMALL_STATE(2)] = 0,
-  [SMALL_STATE(3)] = 4,
-  [SMALL_STATE(4)] = 8,
-  [SMALL_STATE(5)] = 12,
-  [SMALL_STATE(6)] = 16,
+  [SMALL_STATE(3)] = 7,
+  [SMALL_STATE(4)] = 14,
+  [SMALL_STATE(5)] = 21,
+  [SMALL_STATE(6)] = 28,
 };
 
 static const TSParseActionEntry ts_parse_actions[] = {
   [0] = {.entry = {.count = 0, .reusable = false}},
   [1] = {.entry = {.count = 1, .reusable = false}}, RECOVER(),
-  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
-  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(4),
-  [7] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
-  [9] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
-  [11] = {.entry = {.count = 1, .reusable = true}}, SHIFT(6),
-  [13] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_dataset, 4, 0, 0),
+  [3] = {.entry = {.count = 1, .reusable = true}}, SHIFT_EXTRA(),
+  [5] = {.entry = {.count = 1, .reusable = true}}, SHIFT(2),
+  [7] = {.entry = {.count = 1, .reusable = true}}, SHIFT(4),
+  [9] = {.entry = {.count = 1, .reusable = true}},  ACCEPT_INPUT(),
+  [11] = {.entry = {.count = 1, .reusable = true}}, SHIFT(5),
+  [13] = {.entry = {.count = 1, .reusable = true}}, SHIFT(6),
+  [15] = {.entry = {.count = 1, .reusable = true}}, REDUCE(sym_dataset, 4, 0, 0),
 };
 
 #ifdef __cplusplus
